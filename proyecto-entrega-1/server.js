779e7d0cc73c8775c2eaf6d204a477d4d6aec7bb
Fabio Marques <fabio.amarques1990@gmail.com>
2022-12-08 19:52:44 -0600

    Arregle las rutas, quedo mas limpio. Y ahora estoy viendo que el error esta en la funcion getAll() de mi contenedor.

--- proyecto-entrega-1/server.js
+++ proyecto-entrega-1/server.js
@@ -1,16 +1,24 @@
 import express from "express";
-import routerProyecto from  "./routes/proyecto.js";
+import  {Router} from "express";
+import prodRouter from  "./routes/productos.js";
+import carritoRouter from  "./routes/carrito.js";
 
 export const admin = false;
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api", routerProyecto)
+//app.use("/api", routerProyecto)
+
+app.use("/api/productos", prodRouter);
+app.use("/api/carrito", carritoRouter);
 
 app.use((req, res) => {
-    res.json({error, description: "Ruta no existente " + req.originalUrl});
-})
+    res.status(404).json({
+      error: -2,
+      descripcion: `ruta '${req.originalUrl}' método '${req.method}' no implementada`,
+    });
+  });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Listening in ${PORT}`));
